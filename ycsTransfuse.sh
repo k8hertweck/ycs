64a0371943a201520ffb9050d75a6b79b535eb1c
k8hertweck <k8hertweck@gmail.com>
2016-06-15 15:51:20 -0500

    initial commit for transfuse test

---
+++ ycsTransfuse.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/data/ycs/assemblies
+
+# soap2
+transfuse --assemblies soap217.scafSeq,soap227.scafSeq,soap237.scafSeq,soap247.scafSeq,soap257.scafSeq,soap267.scafSeq -left ../left.norm.fq -right ../right.norm.fq --output soap2merged.fa --threads 6
