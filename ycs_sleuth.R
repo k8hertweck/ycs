fee8dfda8b2121646960dc16ef91507b5d4c719c
k8hertweck <k8hertweck@gmail.com>
2016-06-16 11:58:40 -0500

    refining for multiple FV

--- ycs_sleuth.R
+++ ycs_sleuth.R
@@ -19,28 +19,18 @@ sample_id <- dir(file.path(base_dir,"Trinity"))
 ycs_dirs <- sapply(sample_id, function(id) file.path(base_dir, "Trinity", id))
 ycs_dirs
 # table with experimental design
-meta <- read.table("ycsFV6H_info.txt", header = TRUE, stringsAsFactors=FALSE)
+meta <- read.table("ycs_info.txt", header = TRUE, stringsAsFactors=FALSE)
 # enter directories into column 
 meta <- dplyr::mutate(meta, path = ycs_dirs)
 
-## model only treatment
-# construct sleuth object
-so <- sleuth_prep(meta, ~ condition)
-so <- sleuth_fit(so)
+## model treatment + sample
+so <- sleuth_prep(meta, ~ condition + stool + FV )
+so <- sleuth_fit(so2)
 so <- sleuth_wt(so, 'conditionY')
+so <- sleuth_wt(so, 'stool')
+so <- sleuth_wt(so, 'FV' )
 models(so)
-so_results <- sleuth_results(so, 'condition')
-write.table(so_results, "so_results.csv")
+so_results <- sleuth_results(so, 'conditionY')
 saveRDS(so, "so_object.rds")
 so <- readRDS("so_object.rds")
 sleuth_live(so)
-## model treatment + sample
-so2 <- sleuth_prep(meta, ~ condition + stool)
-so2 <- sleuth_fit(so2)
-so2 <- sleuth_wt(so2, 'conditionY')
-so2 <- sleuth_wt(so2, 'stool')
-models(so2)
-so2_results <- sleuth_results(so2, 'conditionY')
-saveRDS(so2, "so2_object.rds")
-so2 <- readRDS("so2_object.rds")
-sleuth_live(so2)
