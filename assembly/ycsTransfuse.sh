31b263d930df3b59de6b540715a431a8dd15d7a9
k8hertweck <k8hertweck@gmail.com>
2018-06-04 14:20:02 -0500

    correcting typos in scripts

--- assembly/ycsTransfuse.sh
+++ assembly/ycsTransfuse.sh
@@ -3,4 +3,5 @@
 cd ~/data/ycs/assemblies
 
 # soap2
-transfuse --assemblies soap217.scafSeq,soap227.scafSeq,soap237.scafSeq,soap247.scafSeq,soap257.scafSeq,soap267.scafSeq -left ../left.norm.fq -right ../right.norm.fq --output soap2merged.fa --threads 6
+transfuse --assemblies soap217.scafSeq,soap227.scafSeq,soap237.scafSeq,soap247.scafSeq,soap257.scafSeq,soap267.scafSeq --left ../left.norm.fq --right ../right.norm.fq --output soap2merged.fa --threads 6
+
